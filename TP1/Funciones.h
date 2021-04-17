67aaa0b65867f156b8eee61ca3b7062d8bdd746d
IgnacioHRegueiro <78211529+IgnacioHRegueiro@users.noreply.github.com>
2021-04-17 19:18:00 -0300

    TP1 Actualizado

---
+++ TP1/Funciones.h
@@ -0,0 +1,23 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+#include <stdio.h>
+
+int GetInt (char mensaje []);
+
+int IngresarOperando(void);
+
+
+int CalcularSuma(int, int);
+
+int CalcularResta(int, int);
+
+float CalcularDivision (int, int);
+
+int CalcularMultiplicacion (int, int);
+
+unsigned long long int CalcularFactorial(int);
+
+
+
+#endif // FUNCIONES_H_INCLUDED
